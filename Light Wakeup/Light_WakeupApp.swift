c8776249f5759e8f29df0752e33144796cf4abc7
HenryRoutson <69740744+HenryRoutson@users.noreply.github.com>
2021-01-05 16:19:32 +1100

    Initial Commit

---
+++ Light Wakeup/Light_WakeupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_WakeupApp.swift
+//  Light Wakeup
+//
+//  Created by Henry on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Light_WakeupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
