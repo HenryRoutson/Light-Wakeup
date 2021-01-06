daf0043f1ea5eb1d3e6ba5b5535da4aacf184bb5
HenryRoutson <69740744+HenryRoutson@users.noreply.github.com>
2021-01-06 18:20:50 +1100

    UI

--- Light Wakeup/ContentView.swift
+++ Light Wakeup/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text("Make sure to have a backup alarm set")
+                .foregroundColor(.gray)
+            Spacer()
+            DatePicker("Select a wakeup time:", selection: .constant(Date()), displayedComponents: [.hourAndMinute])
+                .padding(.all, 30.0)
+            Toggle(isOn: .constant(true)) {
+                Text("On / Off")
+            }
+            .padding(.horizontal, 90.0)
+            .padding(.vertical, 70.0)
+            Link("Support the the developer",
+                 destination: URL(string: "https://www.example.com/TOS.html")!)
+                .padding(.bottom)
+                .padding(.top)
+            Link("Feedback",
+                 destination: URL(string: "https://www.example.com/TOS.html")!)
+                .padding(.bottom)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
