06a051aaa693dbbe2eef525a4f5b4965e76e7ef7
HenryRoutson <69740744+HenryRoutson@users.noreply.github.com>
2021-01-15 19:15:16 +1100

    Bug fixes and simplified UI

--- Light Wakeup/ContentView.swift
+++ Light Wakeup/ContentView.swift
@@ -14,44 +14,51 @@ struct ContentView: View {
     @State private var TimeMinDuration = 5
     
     @State private var NotificationToggle = true
+    @State private var NotificationsSet = false // would be more efficient not to have to use this if you could set notification trigger conditions
     private let NotificationSecTimeInterval = 0.5
 
     init() {
-        print(#function)
+        print("FILTER ",#function)
         
         // ask for notification permission, if not already
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
-            if granted == true && error == nil { print("Notifications permitted") }}
+            if granted == true && error == nil { print("FILTER Notifications permitted") }}
     }
 
-    func WakeupNotifications() {
-        print(#function)
+    func SetWakeupNotifications() {
+        print("FILTER ",#function)
         
-        //define notification
-        let content = UNMutableNotificationContent()
-        content.title = NSString.localizedUserNotificationString(forKey: "Wake Up!", arguments: nil)
-        content.body = NSString.localizedUserNotificationString(forKey: "Clear to stop", arguments: nil)
-        content.sound = UNNotificationSound.defaultCriticalSound(withAudioVolume: 0.01) // makes silent without importing new sounds, while still activating flash
+        // make sure notifications aren't double set
+        if NotificationsSet == false && NotificationToggle == true {
         
-        // create loop to schedule sequential notifications
-        var NotificationTime = WakeupTime
-        for _ in 0...Int(TimeMinDuration*60) {
+            //define notification
+            let content = UNMutableNotificationContent()
+            content.title = NSString.localizedUserNotificationString(forKey: "Wake Up!", arguments: nil)
+            content.body = NSString.localizedUserNotificationString(forKey: "", arguments: nil)
+            content.sound = UNNotificationSound.defaultCriticalSound(withAudioVolume: 0.01) // makes silent without importing new sounds, while still activating flash
             
-            //send notification
-            let dateMatching = Calendar.current.dateComponents([.hour, .minute, .second], from: NotificationTime)
-            let trigger = UNCalendarNotificationTrigger(dateMatching: dateMatching, repeats: true)
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            UNUserNotificationCenter.current().add(request)
-            
-            // add to time for next notification
-            NotificationTime = Date(timeInterval: NotificationSecTimeInterval, since: NotificationTime) // shorter time intervals can stop vibration
-        }
+            // create loop to schedule sequential notifications
+            var NotificationTime = WakeupTime
+            for _ in 0...Int(TimeMinDuration*60) {
+                
+                //send notification
+                let dateMatching = Calendar.current.dateComponents([.hour, .minute, .second], from: NotificationTime)
+                let trigger = UNCalendarNotificationTrigger(dateMatching: dateMatching, repeats: true)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request)
+                
+                // add to time for next notification
+                NotificationTime = Date(timeInterval: NotificationSecTimeInterval, since: NotificationTime) // shorter time intervals can stop vibration
+                }
+            NotificationsSet = true
+            }
     }
     
     func StopWakeupNotifications() {
-        print(#function)
+        print("FILTER ",#function)
         UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        NotificationsSet = false
     }
     
     var body: some View {
@@ -65,22 +72,20 @@ struct ContentView: View {
                        displayedComponents: [.hourAndMinute])
                 .padding([.top, .leading, .trailing], 40.0)
                 .padding(.bottom, 0.0)
-            
-            Button("Confirm time", action: WakeupNotifications)
-                .foregroundColor(.white)
-                .padding(.all)
-                .background(Color.black)
-                .cornerRadius(30)
-                .padding(.bottom, 50)
+                .onChange(of: WakeupTime) { x in
+                    print("FILTER datepicker")
+                    StopWakeupNotifications()
+                    SetWakeupNotifications()
+                }
                 
             
             Toggle("Notification flash\n (if screen is off)", isOn: $NotificationToggle)
                 .padding(.horizontal, 80.0)
                 .padding(.bottom, 100.0)
-                .onChange(of: NotificationToggle) { newValue in
-                    print("toggle")
+                .onChange(of: NotificationToggle) { x in
+                    print("FILTER toggle")
                     if NotificationToggle {
-                        WakeupNotifications()
+                        SetWakeupNotifications()
                     }
                     else {
                         StopWakeupNotifications()
